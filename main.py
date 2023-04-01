2ee4def356c50d3e72c576a506417388f82e581d
Ken Xie <kexie@arista.com>
2023-03-31 21:32:13 -0700

    detectlanguage added to produce correct voice

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from concurrent import futures
 import grpc
 import pb.tts_pb2 as tts_pb2
@@ -5,9 +6,10 @@ from pb.tts_pb2_grpc import add_GoogleTranslateTTSServicer_to_server
 from service.TtsServer import GoogleTranslateTTSService
 
 if __name__ == "__main__":
+    dlapikey = sys.argv[1]
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
     add_GoogleTranslateTTSServicer_to_server(
-        GoogleTranslateTTSService(), server)
+        GoogleTranslateTTSService(dlapikey), server)
     server.add_insecure_port('0.0.0.0:50051')
     server.start()
     server.wait_for_termination()
--- service/TtsServer.py
+++ service/TtsServer.py
@@ -3,15 +3,33 @@ from io import BytesIO
 from pb.tts_pb2 import Response, File
 from pb.tts_pb2_grpc import GoogleTranslateTTSServicer
 from gtts import gTTS
+# from langdetect import detect # total rubbish, works like shxt
+# from googletrans import Translator # not working either
+import detectlanguage
 
 
 class GoogleTranslateTTSService(GoogleTranslateTTSServicer):
+    def __init__(self, detectlanguage_apikey) -> None:
+        super().__init__()
+        detectlanguage.configuration.api_key = detectlanguage_apikey
+
     def TextToSpeech(self, request, context) -> Response:
         text = request.text
-        speech = gTTS(text, lang="en")
+        try:
+            language = self.GetLang(text)
+        except:
+            language = "en"
+        speech = gTTS(text, lang=language)
 
         buffer = BytesIO()
         speech.write_to_fp(buffer)
         buffer.seek(0)
 
         return Response(success=True, fileResponse=File(name="speech.mp3", data=buffer.read()))
+
+    def GetLang(self, text):
+        detected = detectlanguage.detect(text)
+        lang = detected[0]["language"]
+        if lang == 'zh-Hant':
+            lang = 'zh-TW'
+        return lang
